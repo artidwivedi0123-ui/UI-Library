560d0aac554d7debb3e904005dfec132ddcd51e5
Arti Dwivedi <artidwivedi0123@gmail.com>
2026-07-11 16:40:25 +0530

    Card Updating

--- src/components/layout/Card/CardHeader.tsx
+++ src/components/layout/Card/CardHeader.tsx
@@ -1,20 +1,20 @@
-import * as React from "react";
-import { cn } from "@/utils/cn";
+// import * as React from "react";
+// import { cn } from "@/utils/cn";
 
-const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
-    className={cn(
-      "flex flex-col gap-[var(--cuix-spacing-xs)]",
-      className
-    )}
-    {...props}
-  />
-));
+// const CardHeader = React.forwardRef<
+//   HTMLDivElement,
+//   React.HTMLAttributes<HTMLDivElement>
+// >(({ className, ...props }, ref) => (
+//   <div
+//     ref={ref}
+//     className={cn(
+//       "flex flex-col gap-[var(--cuix-spacing-xs)]",
+//       className
+//     )}
+//     {...props}
+//   />
+// ));
 
-CardHeader.displayName = "CardHeader";
+// CardHeader.displayName = "CardHeader";
 
-export { CardHeader };
+// export { CardHeader };
