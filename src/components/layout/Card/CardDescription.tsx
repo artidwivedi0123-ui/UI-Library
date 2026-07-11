32e6f42db5e27968853a6210e7e66156d2861d53
Arti Dwivedi <artidwivedi0123@gmail.com>
2026-07-11 16:31:36 +0530

    Card Updating

--- src/components/layout/Card/CardDescription.tsx
+++ src/components/layout/Card/CardDescription.tsx
@@ -1,20 +1,20 @@
-// import * as React from "react";
-// import { cn } from "@/utils/cn";
+import * as React from "react";
+import { cn } from "@/utils/cn";
 
-// const CardDescription = React.forwardRef<
-//   HTMLParagraphElement,
-//   React.HTMLAttributes<HTMLParagraphElement>
-// >(({ className, ...props }, ref) => (
-//   <p
-//     ref={ref}
-//     className={cn(
-//       "text-[var(--cuix-font-size-sm)] text-[var(--cuix-colors-muted-foreground)]",
-//       className
-//     )}
-//     {...props}
-//   />
-// ));
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn(
+      "text-[var(--cuix-font-size-sm)] text-[var(--cuix-colors-muted-foreground)]",
+      className
+    )}
+    {...props}
+  />
+));
 
-// CardDescription.displayName = "CardDescription";
+CardDescription.displayName = "CardDescription";
 
-// export { CardDescription };
+export { CardDescription };
--- src/components/layout/Card/index.ts
+++ src/components/layout/Card/index.ts
@@ -1,10 +1,8 @@
-// export { Card } from "@/components/layout/Card/Card";
-// // export { CardHeader } from "@/components/layout/Card/CardHeader";
-// // export { CardTitle } from "@/components/layout/Card/CardTitle";
-// // export { CardDescription } from "@/components/layout/Card/CardDescription";
-// // export { CardContent } from "@/components/layout/Card/CardContent";
-// // export { CardFooter } from "@/components/layout/Card/CardFooter";
+export { Card } from "@/components/layout/Card/Card";
+export { CardHeader } from "@/components/layout/Card/CardHeader";
+export { CardTitle } from "@/components/layout/Card/CardTitle";
+export { CardDescription } from "@/components/layout/Card/CardDescription";
+export { CardContent } from "@/components/layout/Card/CardContent";
+export { CardFooter } from "@/components/layout/Card/CardFooter";
 
-// export * from "@/components/layout/Card/Card.type";
-
-export * from  "@/components/layout/Card";
+export * from "@/components/layout/Card/Card.type";
